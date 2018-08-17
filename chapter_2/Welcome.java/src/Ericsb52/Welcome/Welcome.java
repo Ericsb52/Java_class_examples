056bf632effb3391210c01d5a3f62466db8a54a0
broad <broad@10.208.96.33>
2018-08-17 13:40:10 -0600

    first commit

--- chapter_2/Welcome.java/src/Ericsb52/Welcome/Welcome.java
+++ chapter_2/Welcome.java/src/Ericsb52/Welcome/Welcome.java
@@ -14,7 +14,8 @@ public class Welcome {
 	 */
 	public static void main(String[] args) {
 		// Display message welcome to java! on the console
-		System.out.println("Welcome to Java!");
+		System.out.println("Welcome to Java!"); 
+		
 
 	}
 
