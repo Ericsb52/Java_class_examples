83dd60af2cc95b64c0c9cc59adfb32385708c632
broad <broad@10.208.96.33>
2018-08-17 13:28:30 -0600

    first commit chapter 2 example 1

---
+++ chapter_2/Welcome.java/src/Ericsb52/Welcome/Welcome.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package Ericsb52.Welcome;
+
+/**
+ * @author broad
+ *
+ */
+public class Welcome {
+
+	/**
+	 * chapter 2 example 1
+	 */
+	public static void main(String[] args) {
+		// Display message welcome to java! on the console
+		System.out.println("Welcome to Java!");
+
+	}
+
+}
